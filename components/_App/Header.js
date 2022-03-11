acf23f8207ea0b08aff1727c9cb59b126941d239
YSJ <applotnwjd@gmail.com>
2022-03-11 13:28:44 +0900

    Header Link

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -109,15 +109,17 @@ const Header = () => {
         <div className="max-w-[100%] mx-auto px-4 sm:px-6">
           <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
             <div className="flex justify-start lg:w-0 lg:flex-1">
-              <a href="#">
-                <span className="sr-only">Cube Entertainment</span>
-                {/* <Image
+              <Link href="#">
+                <a>
+                  <span className="sr-only">Cube Entertainment</span>
+                  {/* <Image
                   src={baseApiUrl + logo.url}
                   alt={logo.alternativeText}
                   width={logo.width}
                   height={logo.height}
                 /> */}
-              </a>
+                </a>
+              </Link>
             </div>
             <div className="-mr-2 -my-2 md:hidden">
               <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -158,39 +160,39 @@ const Header = () => {
                         <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                           <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
                             {solutions.map((item) => (
-                              <a
-                                key={item.name}
-                                href={item.href}
-                                className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
-                              >
-                                <item.icon
-                                  className="flex-shrink-0 h-6 w-6 text-indigo-600"
-                                  aria-hidden="true"
-                                />
-                                <div className="ml-4">
-                                  <p className="text-base font-medium text-gray-900">
-                                    {item.name}
-                                  </p>
-                                  <p className="mt-1 text-sm text-gray-500">
-                                    {item.description}
-                                  </p>
-                                </div>
-                              </a>
+                              <Link href={item.href}>
+                                <a
+                                  key={item.name}
+                                  className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                                >
+                                  <item.icon
+                                    className="flex-shrink-0 h-6 w-6 text-indigo-600"
+                                    aria-hidden="true"
+                                  />
+                                  <div className="ml-4">
+                                    <p className="text-base font-medium text-gray-900">
+                                      {item.name}
+                                    </p>
+                                    <p className="mt-1 text-sm text-gray-500">
+                                      {item.description}
+                                    </p>
+                                  </div>
+                                </a>
+                              </Link>
                             ))}
                           </div>
                           <div className="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
                             {callsToAction.map((item) => (
                               <div key={item.name} className="flow-root">
-                                <a
-                                  href={item.href}
-                                  className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100"
-                                >
-                                  <item.icon
-                                    className="flex-shrink-0 h-6 w-6 text-gray-400"
-                                    aria-hidden="true"
-                                  />
-                                  <span className="ml-3">{item.name}</span>
-                                </a>
+                                <Link href={item.href}>
+                                  <a className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100">
+                                    <item.icon
+                                      className="flex-shrink-0 h-6 w-6 text-gray-400"
+                                      aria-hidden="true"
+                                    />
+                                    <span className="ml-3">{item.name}</span>
+                                  </a>
+                                </Link>
                               </div>
                             ))}
                           </div>
@@ -200,19 +202,16 @@ const Header = () => {
                   </>
                 )}
               </Popover>
-
-              <a
-                href="#"
-                className="text-xl font-medium text-gray-500 hover:text-gray-900"
-              >
-                CUBE
-              </a>
-              <a
-                href="#"
-                className="text-xl font-medium text-gray-500 hover:text-gray-900"
-              >
-                Notice
-              </a>
+              <Link href="#">
+                <a className="text-xl font-medium text-gray-500 hover:text-gray-900">
+                  CUBE
+                </a>
+              </Link>
+              <Link href="#">
+                <a className="text-xl font-medium text-gray-500 hover:text-gray-900">
+                  Notice
+                </a>
+              </Link>
 
               {/*<Popover className="relative">
                 {({ open }) => (
@@ -306,18 +305,16 @@ const Header = () => {
                                 </Popover>*/}
             </Popover.Group>
             <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-              <a
-                href="#"
-                className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-              >
-                Sign in
-              </a>
-              <a
-                href="#"
-                className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-cyan-500 hover:bg-cyan-600"
-              >
-                Sign up
-              </a>
+              <Link href="#">
+                <a className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                  Sign in
+                </a>
+              </Link>
+              <Link href="#">
+                <a className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-cyan-500 hover:bg-cyan-600">
+                  Sign up
+                </a>
+              </Link>
             </div>
           </div>
         </div>
@@ -359,63 +356,60 @@ const Header = () => {
                 <div className="mt-6">
                   <nav className="grid gap-y-8">
                     {solutions.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                      >
-                        <item.icon
-                          className="flex-shrink-0 h-6 w-6 text-indigo-600"
-                          aria-hidden="true"
-                        />
-                        <span className="ml-3 text-base font-medium text-gray-900">
-                          {item.name}
-                        </span>
-                      </a>
+                      <Link href={item.href}>
+                        <a
+                          key={item.name}
+                          className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                        >
+                          <item.icon
+                            className="flex-shrink-0 h-6 w-6 text-indigo-600"
+                            aria-hidden="true"
+                          />
+                          <span className="ml-3 text-base font-medium text-gray-900">
+                            {item.name}
+                          </span>
+                        </a>
+                      </Link>
                     ))}
                   </nav>
                 </div>
               </div>
               <div className="py-6 px-5 space-y-6">
                 <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                  <a
-                    href="#"
-                    className="text-base font-medium text-gray-900 hover:text-gray-700"
-                  >
-                    Pricing
-                  </a>
-
-                  <a
-                    href="#"
-                    className="text-base font-medium text-gray-900 hover:text-gray-700"
-                  >
-                    Docs
-                  </a>
-                  {resources.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="text-base font-medium text-gray-900 hover:text-gray-700"
-                    >
-                      {item.name}
+                  <Link href="#">
+                    <a className="text-base font-medium text-gray-900 hover:text-gray-700">
+                      Pricing
                     </a>
+                  </Link>
+                  <Link href="#">
+                    <a className="text-base font-medium text-gray-900 hover:text-gray-700">
+                      Docs
+                    </a>
+                  </Link>
+                  {resources.map((item) => (
+                    <Link href={item.href}>
+                      <a
+                        key={item.name}
+                        className="text-base font-medium text-gray-900 hover:text-gray-700"
+                      >
+                        {item.name}
+                      </a>
+                    </Link>
                   ))}
                 </div>
                 <div>
-                  <a
-                    href="#"
-                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                  >
-                    Sign up
-                  </a>
+                  <Link href="#">
+                    <a className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                      Sign up
+                    </a>
+                  </Link>
                   <p className="mt-6 text-center text-base font-medium text-gray-500">
                     Existing customer?{" "}
-                    <a
-                      href="#"
-                      className="text-indigo-600 hover:text-indigo-500"
-                    >
-                      Sign in
-                    </a>
+                    <Link href="#">
+                      <a className="text-indigo-600 hover:text-indigo-500">
+                        Sign in
+                      </a>
+                    </Link>
                   </p>
                 </div>
               </div>
