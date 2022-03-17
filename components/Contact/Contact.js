feed3c3e0cf4983efcb6c7a3b066b5d9151f8baa
YSJ <applotnwjd@gmail.com>
2022-03-17 10:18:17 +0900

    contact style

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -9,14 +9,6 @@ const Contact = () => {
   const [content, setContent] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
-    // if (name === null) {
-    //   alert("이름을 입력해주세요.");
-    //   return false;
-    // }
-    // if (email === null) {
-    //   alert("이메일을 입력해주세요.");
-    //   return false;
-    // }
     const data = {
       name,
       email,
@@ -45,61 +37,87 @@ const Contact = () => {
     fetch("/api/contact", {
       method: "post",
       body: JSON.stringify(data),
-    })
-      .then((res) => {
-        if (res.status === 200) {
-          alert("접수되었습니다.");
-          return router.push("/");
-        } else {
-          alert("접수를 실패하였습니다.");
-        }
-      })
-      .then((res) => {
-        console.log("!!!", res);
-      });
+    }).then((res) => {
+      if (res.status === 200) {
+        alert("접수되었습니다.");
+        return router.push("/");
+      } else {
+        alert("접수를 실패하였습니다.");
+      }
+    });
   };
-
-  //   const confirm = () => {
-  //     const confirmOk = confirm("접수가 완료되었습니다.");
-  //     if (confirmOk) {
-  //       router.push("/");
-  //     } else {
-  //       return false;
-  //     }
-  //   };
   return (
-    <div className="contact">
-      <form name="contact-form" onSubmit={handleSubmit}>
-        <label htmlFor="name">이름</label>
-        <input
-          id="name"
-          name="name"
-          required
-          type="text"
-          className="border border-1 border-gray-400"
-          onChange={(e) => setName(e.target.value)}
-        />
-
-        <label htmlFor="email">이메일</label>
-        <input
-          id="email"
-          name="email"
-          required
-          type="email"
-          className="border border-1 border-gray-400"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <label htmlFor="content">내용</label>
-        <textarea
-          id="content"
-          name="content"
-          className="border border-1 border-gray-400"
-          onChange={(e) => setContent(e.target.value)}
-        ></textarea>
-
-        <button type="submit">문의하기</button>
-      </form>
+    <div className="sub justify-self-center w-full">
+      <div className="subVisual relative h-60 bg-sky-500">
+        <p className="subVisText absolute left-2/4 top-2/4 -translate-y-1/2 -translate-x-1/2 z-10 text-white text-center">
+          <span className="text-5xl font-bold block pb-4">Contact</span>
+          <span className="block text-lg font-semibold">
+            궁금하신 점이 있으시다면 문의주세요.
+          </span>
+        </p>
+      </div>
+      <div className="container xl my-0 mx-auto p-7 max-w-3xl	">
+        <div className="contact bg-white">
+          <form name="contact-form" onSubmit={handleSubmit}>
+            <ul>
+              <li className="py-6">
+                <label
+                  htmlFor="name"
+                  className="w-24 inline-block align-middle font-semibold text-lg"
+                >
+                  이름
+                </label>
+                <input
+                  id="name"
+                  name="name"
+                  required
+                  type="text"
+                  className="border-b border-1 border-gray-400 align-middle w-10/12 p-3"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </li>
+              <li className="py-6">
+                <label
+                  htmlFor="email"
+                  className="w-24 inline-block align-middle font-semibold text-lg"
+                >
+                  이메일
+                </label>
+                <input
+                  id="email"
+                  name="email"
+                  required
+                  type="email"
+                  className="border-b border-1 border-gray-400 align-middle w-10/12 p-3"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </li>
+              <li className="py-6">
+                <label
+                  htmlFor="content"
+                  className="w-24 inline-block align-middle font-semibold text-lg"
+                >
+                  내용
+                </label>
+                <textarea
+                  id="content"
+                  name="content"
+                  className="border border-1 border-gray-400 align-middle w-10/12 rounded-lg p-3"
+                  onChange={(e) => setContent(e.target.value)}
+                ></textarea>
+              </li>
+            </ul>
+            <div className="text-center">
+              <button
+                type="submit"
+                className="p-3 text-white font-semibold bg-sky-500 inline-block rounded-xl hover:bg-sky-600"
+              >
+                문의하기
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
