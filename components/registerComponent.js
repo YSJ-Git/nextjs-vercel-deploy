41d7db05d941318ef82355a8a8e0e289aed77d30
YSJ-Git <applotnwjd@gmail.com>
2022-05-09 12:05:26 +0900

    email login

--- components/registerComponent.js
+++ components/registerComponent.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
@@ -119,59 +118,3 @@ const RegisterComponent = () => {
 };
 
 export default RegisterComponent;
-=======
-import { useState } from 'react'
-import { useRouter } from 'next/router'
-import axios from 'axios'
-
-const RegisterComponent = () => {
-  const router = useRouter()
-  const [userData, setUserData] = useState({
-    username: '',
-    email: '',
-    password: '',
-  })
-
-  const handleSubmit = async e => {
-    e.preventDefault()
-    try {
-      await axios.post('/api/register', userData)
-      router.replace('/profile')
-    } catch (err) {
-      console.log(err.response.data)
-    }
-  }
-
-  const handleChange = e => {
-    const { name, value } = e.target
-    setUserData({ ...userData, [name]: value })
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input type="text" name="username" onChange={e => handleChange(e)} />
-      </label>
-      <br />
-      <label>
-        Email:
-        <input type="text" name="email" onChange={e => handleChange(e)} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type="password"
-          name="password"
-          onChange={e => handleChange(e)}
-        />
-      </label>
-      <br />
-      <button>Register</button>
-    </form>
-  )
-}
-
-export default RegisterComponent
->>>>>>> a8d67ad582ea6fd89079231165718683294a283d
--- pages/register.js
+++ pages/register.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-
-import RegisterComponent from '../components/registerComponent'
-const Register = () => <RegisterComponent />
-export default Register
-=======
-import RegisterComponent from '../components/registerComponent'
-const Register = () => <RegisterComponent />
-export default Register
->>>>>>> a8d67ad582ea6fd89079231165718683294a283d
+import RegisterComponent from "../components/registerComponent";
+const Register = () => <RegisterComponent />;
+export default Register;
