0291d143913d7c70b1a8640a3094a9d8166ba8b4
YSJ-Git <applotnwjd@gmail.com>
2022-05-09 12:13:09 +0900

    login logout

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from "axios";
 import { encode } from "next-auth/jwt";
 import { setCookie } from "nookies";
@@ -6,20 +5,17 @@ import baseApiUrl from "../../utils/baseApiUrl";
 
 export default async (req, res) => {
   const { password, identifier } = req.body;
-=======
 import axios from 'axios'
 import { setCookie } from 'nookies'
 import baseApiUrl from '../../utils/baseApiUrl'
 
 export default async (req, res) => {
   const { password, identifier } = req.body
->>>>>>> a8d67ad582ea6fd89079231165718683294a283d
 
   try {
     const postRes = await axios.post(`${baseApiUrl}/api/auth/local`, {
       identifier,
       password,
-<<<<<<< HEAD
     });
 
     setCookie({ res }, "jwt", postRes.data.jwt, {
@@ -34,7 +30,6 @@ export default async (req, res) => {
     res.status(400).send(e.response.data);
   }
 };
-=======
     })
 
     setCookie({ res }, 'jwt', postRes.data.jwt, {
@@ -49,4 +44,3 @@ export default async (req, res) => {
     res.status(400).send(e.response.data)
   }
 }
->>>>>>> a8d67ad582ea6fd89079231165718683294a283d
--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { destroyCookie } from "nookies";
 
 export default async (req, res) => {
@@ -8,14 +7,3 @@ export default async (req, res) => {
 
   res.status(200).end();
 };
-=======
-import { destroyCookie } from 'nookies'
-
-export default async (req, res) => {
-  destroyCookie({ res }, 'jwt', {
-    path: '/',
-  })
-
-  res.status(200).end()
-}
->>>>>>> a8d67ad582ea6fd89079231165718683294a283d
