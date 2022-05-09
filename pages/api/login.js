0da53a151bcf2a1aa72d5eae843e0437d331dd7a
YSJ-Git <applotnwjd@gmail.com>
2022-05-09 12:18:39 +0900

    login

--- pages/api/login.js
+++ pages/api/login.js
@@ -5,12 +5,6 @@ import baseApiUrl from "../../utils/baseApiUrl";
 
 export default async (req, res) => {
   const { password, identifier } = req.body;
-import axios from 'axios'
-import { setCookie } from 'nookies'
-import baseApiUrl from '../../utils/baseApiUrl'
-
-export default async (req, res) => {
-  const { password, identifier } = req.body
 
   try {
     const postRes = await axios.post(`${baseApiUrl}/api/auth/local`, {
@@ -30,17 +24,3 @@ export default async (req, res) => {
     res.status(400).send(e.response.data);
   }
 };
-    })
-
-    setCookie({ res }, 'jwt', postRes.data.jwt, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      maxAge: 30 * 24 * 60 * 60,
-      path: '/',
-    })
-
-    res.status(200).end()
-  } catch (e) {
-    res.status(400).send(e.response.data)
-  }
-}
